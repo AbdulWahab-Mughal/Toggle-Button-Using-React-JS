b8cc95f52b1008d83d103be50b90472c59101b1e
AbdulWahab-Mughal <wahaba374@gmail.com>
2023-08-23 13:22:55 -0700

    toggleBtn

---
+++ src/Components/Toggle.jsx
@@ -0,0 +1,22 @@
+import './Toggle.css'
+import React,{useState} from 'react'
+// import Image from "../assets/Images/dark.png" 
+// import Image from "../assets/Images/light.png" 
+const Toggle=()=>{
+    
+    const[toggle,setToggle]=useState(false);
+    const onClickHandler=()=>{
+        setToggle(!toggle)
+        }
+    return<>
+    <body className={`${toggle? "Dark": "Light"}`}>
+    <h1 className={`${toggle? "darkMode":"lightMode"}`}>{`${toggle? "Dark-Mode":"Light-Mode"}`}</h1>
+    <div className="center">
+        <input type="checkbox" onClick={onClickHandler}/>
+    </div>
+        
+    </body>
+    </>
+}
+
+export default Toggle
